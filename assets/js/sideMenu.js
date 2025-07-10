3862784bcde5cb1fcb7c269338f22ff3b638adc5
Peter <peterhsj@gmail.com>
2025-07-10 15:35:50 +0800

    init

---
+++ assets/js/sideMenu.js
@@ -0,0 +1,26 @@
+// 選單 ID 
+const menuList = ['todoList', 'info', 'lcList', 'negotiationList', 'acceptNegotiation', 'checkNegotiation', 'setGroup', 'setBeneficiary', 'setLcTable', 'accountManager', 'roleManager', 'branchManager', 'track', 'send'];
+
+menuList.forEach(menu => {
+  this[`${menu}tab`] = document.querySelector(`#${menu}`);
+  
+  // 監聽連結
+  if (this[`${menu}tab`]) {
+    this[`${menu}tab`].addEventListener('click', (event) => {
+      event.preventDefault();
+      
+      // 樣式切換
+      clearAction();
+      event.target.classList.add('active');  
+      document.querySelector('#main-content').src = `./pages/${menu}.html`;
+    });
+  }
+});
+
+// 清除選單全部 [.active] 樣式
+function clearAction() {
+  menuList.forEach(menu => {
+    this[`${menu}tab`] = document.querySelector(`#${menu}`);
+    this[`${menu}tab`].classList.remove('active');
+  });
+};
